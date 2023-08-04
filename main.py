396e6ae91dc03390361783b1b4dea4887a0b5f73
nbalcarc <nathan.balcarcel@wsu.edu>
2023-08-03 19:05:17 -0700

    PyTorch confirmed to be working

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import testing
 
 def main():
     """Main entry point."""
-    testing.torch_testing()
+    #testing.torch_testing()
+    testing.torch_testing_basic()
 
 
 if __name__ == "__main__":
