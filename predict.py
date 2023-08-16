c1b5822651fe26eeaf3c0f2b03f02e0aca786860
nbalcarc <nathan.balcarcel@wsu.edu>
2023-08-16 12:41:46 -0700

    Added learning for all lte and vector configurations, six total

--- predict.py
+++ predict.py
@@ -10,73 +10,124 @@ def predict_data():
     state_vectors, vectors, lte = data.load_data()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(device)
+    print(f"Running on device: {device}")
 
     state_vectors = torch.from_numpy(state_vectors)
     vectors = torch.from_numpy(vectors)
     lte = torch.from_numpy(lte)
 
-    lte_10 = lte[:, 0, :] #0 for 10, 1 for 30, 2 for 50
-    print(lte_10.shape)
+    lte_10 = lte[:, 0, :] #0 for lte10, 1 for lte30, 2 for lte50
+    lte_30 = lte[:, 1, :]
+    lte_50 = lte[:, 2, :]
+    #print(lte_10.shape)
 
-    # training on vectors (size 1024)
-    X_train = vectors[:31].reshape(-1, 1024).to(device)
-    y_train = lte_10[:31].reshape(-1, 1).to(device)
-    X_test = vectors[31].reshape(-1, 1024).to(device)
-    y_test = lte_10[31].reshape(-1, 1).to(device)
+    for i in range(6):
+        match i:
+            case 0:
+                cur_lte = lte_10
+                cur_vec = state_vectors
+            case 1:
+                cur_lte = lte_30
+                cur_vec = state_vectors
+            case 2:
+                cur_lte = lte_50
+                cur_vec = state_vectors
+            case 3:
+                cur_lte = lte_10
+                cur_vec = vectors
+            case 4:
+                cur_lte = lte_30
+                cur_vec = vectors
+            case _:
+                cur_lte = lte_50
+                cur_vec = vectors
 
-    print(X_train.shape)
-    print(y_train.shape)
+        if i < 3:
+            # training on state_vectors (size 2048)
+            X_train = cur_vec[:31].reshape(-1, 2048).to(device)
+            y_train = cur_lte[:31].reshape(-1, 1).to(device)
+            X_test = cur_vec[31].reshape(-1, 2048).to(device)
+            y_test = cur_lte[31].reshape(-1, 1).to(device)
 
-    print(y_train)
+            model = nn.Sequential(
+                    nn.Linear(2048, 2048),
+                    nn.ReLU(),
+                    nn.Linear(2048, 512),
+                    nn.ReLU(),
+                    nn.Linear(512, 1),
+                    ).to(device)
+        else:
+            # training on vectors (size 1024)
+            X_train = cur_vec[:31].reshape(-1, 1024).to(device)
+            y_train = cur_lte[:31].reshape(-1, 1).to(device)
+            X_test = cur_vec[31].reshape(-1, 1024).to(device)
+            y_test = cur_lte[31].reshape(-1, 1).to(device)
 
-    model = nn.Sequential(
-        nn.Linear(1024, 1024),
-        nn.ReLU(),
-        nn.Linear(1024, 512),
-        nn.ReLU(),
-        nn.Linear(512, 1),
-        #nn.Sigmoid()
-        ).to(device)
-    print(model)
+            model = nn.Sequential(
+                    nn.Linear(1024, 1024),
+                    nn.ReLU(),
+                    nn.Linear(1024, 512),
+                    nn.ReLU(),
+                    nn.Linear(512, 1),
+                    ).to(device)
 
-    #loss_fn = nn.BCELoss()
-    loss_fn = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr = 0.0001)
+        #print(model)
+        #print(X_train.shape)
+        #print(y_train.shape)
+        #print(y_train)
 
-    n_epochs = 100
-    history = np.zeros(n_epochs)
-    batch_size = 10
-    loss = 0  #ease type errors
+        #loss_fn = nn.BCELoss()
+        loss_fn = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr = 0.0001)
 
-    #training loop
-    for epoch in range(n_epochs):
-        model.train()
-        for i in range(0, len(X_train), batch_size):
-            # initialize batches
-            X_batch = X_train[i:i+batch_size]
-            y_batch = y_train[i:i+batch_size]
+        n_epochs = 100
+        history = np.zeros(n_epochs)
+        batch_size = 10
+        loss = 0  #ease type errors
 
-            # forward pass
-            y_pred = model(X_batch)
-            loss = loss_fn(y_pred, y_batch)
+        print(f"=== Training model {i} of 6 === ")
 
-            # backward pass
-            optimizer.zero_grad()
-            loss.backward()
+        #training loop
+        for epoch in range(n_epochs):
+            model.train()
+            for i in range(0, len(X_train), batch_size):
+                # initialize batches
+                X_batch = X_train[i:i+batch_size]
+                y_batch = y_train[i:i+batch_size]
 
-            optimizer.step()  #update weights
+                # forward pass
+                y_pred = model(X_batch)
+                loss = loss_fn(y_pred, y_batch)
 
-        model.eval()
-        y_pred = model(X_test)
-        true_loss = loss_fn(y_pred, y_test)
-        print(f'Finished epoch {epoch}, latest loss {true_loss}')
-        history[epoch] = true_loss
+                # backward pass
+                optimizer.zero_grad()
+                loss.backward()
 
-    # graph results
-    plt.plot(np.arange(n_epochs), history, label = "original")
-    plt.title("loss over time")
-    plt.savefig("output.png")
+                optimizer.step()  #update weights
+
+            model.eval()
+            y_pred = model(X_test)
+            true_loss = loss_fn(y_pred, y_test)
+            print(f'Finished epoch {epoch}, latest loss {true_loss}')
+            history[epoch] = true_loss
+
+        # graph results
+        plt.plot(np.arange(n_epochs), history, label = "original")
+        plt.title("Loss Over Time")
+
+        file = ""
+        if i < 3:
+            file += "output_state_vectors_"
+        else:
+            file += "output_vectors_"
+        if i % 3 == 0:
+            file += "lte10.png"
+        elif i % 3 == 1:
+            file += "lte30.png"
+        else:
+            file += "lte50.png"
+            
+        plt.savefig(file)
 
 
 
