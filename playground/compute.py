44d5cf00b9c641817985f2f89bfd7f22e6c35ab2
nbalcarc <nathan.balcarcel@wsu.edu>
2023-09-18 17:52:18 -0700

    Cleaned up comments

--- playground/compute.py
+++ playground/compute.py
@@ -33,24 +33,13 @@ def similarity():
                 incr += 1
                 df[col.DORMANT_DAY][i] = incr
 
-
-
-
     #df.to_csv("output.csv") //output file with numbered dormancy days
-
-
-    # grab all the rows that are part of the dormancy season
-    #dormancy_filtered = df[df[col.DORMANT_SEASON] == 1]
-    #print(dormancy_filtered)
-
 
     # grab all the rows with phenology data
     df_temp = df.copy()
     df_temp[col.PHENOLOGY].fillna(0, inplace=True)
     phenology = df_temp[df_temp[col.PHENOLOGY] != 0]
     phenology = phenology[phenology[col.DORMANT_SEASON] == 1] #filter to only within the dormancy season
-    #print(phenology[col.PHENOLOGY])
-
 
     '''
     Notes:
@@ -84,14 +73,11 @@ def similarity():
         cur_phenologies: pd.DataFrame = phenology[phenology[col.SEASON] == cur_season] #filter to only the current season
         cur_phenologies = cur_phenologies[[col.PHENOLOGY, col.DORMANT_DAY]] #only care about phenology and the day
 
+        # simplifies the process of printing phenology data
         def insert_phenology(y_coordinate: float):
             for row in cur_phenologies.iterrows():
-                #print(row[1])
-                #print(type(row[1]))
-                #print(row[1][0])
-                #print(row[1][1])
                 plt.axvline(row[1][1], color = "red") #graph at the specified index
-                plt.text(row[1][1], y_coordinate, row[1][0], rotation=90)
+                plt.text(row[1][1], y_coordinate, row[1][0], rotation=90) #add a phenology label
 
         # comparisons
         euclidean_distances = np.zeros(251)
@@ -118,68 +104,6 @@ def similarity():
         insert_phenology(1450)
         plt.savefig("output_graphs/cosine_" + cur_season + ".png")
 
-    #cur_vecs: npt.NDArray[np.float32] = state_vectors[0]
-
-    #angle_mags = np.zeros((252, 2048))
-    #euclidean_distances = np.zeros(251)
-    #cosine_similarities = np.zeros(251)
-    ##deltas = np.zeros(250)
-
-    ## individual vector processing
-    #for i in range(252):
-    #    angle_mag = np.zeros(2048)
-    #    for j in range(2047): #want to leave the final index empty, that's where the magnitude goes
-    #        angle_mag[j] = np.arctan2(cur_vecs[i][j], cur_vecs[i][j+1])
-    #    angle_mag[2047] = np.linalg.norm(cur_vecs[i])
-    #    angle_mags[i] = angle_mag
-
-    ## comparative vector processing
-    #for i in range(251):
-    #    euclidean_distances[i] = np.linalg.norm((cur_vecs[i]-cur_vecs[i+1]))
-    #    cosine_similarities[i] = np.dot(cur_vecs[i], cur_vecs[i+1]) / np.linalg.norm(cur_vecs[i]) * np.linalg.norm(cur_vecs[i+1])
-
-    ## further processing on previous loop
-    ##for i in range(250):
-    ##    deltas[i] = cosine_similarities[i+1] - cosine_similarities[i]
-
-    ##print("Euclidean distances:")
-    ##print(euclidean_distances)
-    ##for i in range(251):
-    ##    print(f"{i}: {euclidean_distances[i]}")
-
-    ##print("Cosine similarity:")
-    ##print(cosine_similarities)
-    ##for i in range(251):
-    ##    print(f"{i}: {cosine_similarities[i]}")
-
-    ##print("Cosine deltas:")
-    ##for i in range(250):
-    ##    print(f"{i}: {deltas[i]}")
-
-    ##print("Angles:")
-    ##print(angle_mags)
-
-
-    #'''
-    #TODO:
-    #graph the euclidean distances and see if there's any patterns for where the peaks and flatlands are
-    #find the delta between the cosine similarities and see if there's any drops or rises
-    #'''
-
-    #plt.clf()
-    #plt.plot(list(range(1,251)), euclidean_distances[1:])
-    #plt.title("Euclidean Distances")
-    #plt.savefig("euclidean.png")
-
-    #plt.clf()
-    #plt.plot(list(range(1,251)), cosine_similarities[1:])
-    #plt.title("Cosine Similarities")
-    #plt.savefig("cosine.png")
-
-    ##plt.clf()
-    ##plt.plot(list(range(250)), deltas)
-    ##plt.title("Cosine Deltas")
-    ##plt.savefig("deltas.png")
 
 
 
