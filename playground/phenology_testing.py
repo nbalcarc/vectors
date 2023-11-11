aacf758b52e986e970123b3709ca6bdd3992edfe
nbalcarc <nathan.balcarcel@wsu.edu>
2023-11-10 21:30:41 -0800

    Extracted cultivar and overall phenology stats

--- playground/phenology_testing.py
+++ playground/phenology_testing.py
@@ -1,27 +1,47 @@
-import numpy as np
+#import numpy as np
 import pandas as pd
 import os
+#from functools import reduce
 
-def run_stats():
+def run_stats() -> tuple[dict[str, dict[str, int]], dict[str, int]]:
     all_files = os.listdir("inputs/datasets")
     cultivar_files = list(map(lambda x: "inputs/datasets/" + x, all_files))
     cultivars = list(map(lambda x: x[x.rfind("_")+1:x.rfind(".")], all_files))
-    print(cultivar_files)
-    print(cultivars)
 
-    phenologies = dict()
+    # first collect all phenology tables and find all unique phenologies
+    phenologies: dict[str, pd.Series] = dict()
+    all_phenologies: set[str] = set()
 
     for i in range(len(cultivar_files)):
-        df = pd.read_csv(cultivar_files[i])
-        phenologies[cultivars[i]] = df["PHENOLOGY"]
+        df: pd.DataFrame = pd.read_csv(cultivar_files[i])
+        phenologies[cultivars[i]] = df["PHENOLOGY"] #panda series
+        all_phenologies |= set(df["PHENOLOGY"].dropna()) #union of two sets
 
-    print(phenologies["Riesling"])
+    # collect stats for each cultivar
+    cultivar_stats: dict[str, dict[str, int]] = dict() #cultivar; phenology; count
+
+    for k in cultivars:
+        cur_phens: list[str] = list(phenologies[k].dropna()) #phenologies for current cultivar
+        phen_stats: dict[str, int] = dict()
+        for p in all_phenologies:
+            phen_stats[p] = cur_phens.count(p)
+        cultivar_stats[k] = phen_stats
+
+    # now generate overall stats
+    all_stats = {k:0 for k in all_phenologies} #phenology; count
+    z = list(map(lambda x: list(x.items()), cultivar_stats.values())) #get every cultivar's phenology data
+    for p in z[1:]: #concatenate all phenology data into one list
+        z[0].extend(p)
+    for t in z[0]: #sum all counts
+        all_stats[t[0]] += t[1]
+
+    return cultivar_stats, all_stats
 
 
 def main():
     """Main entry point."""
-    run_stats()
-    pass
+    cultivar_stats, all_stats = run_stats()
+    print(all_stats)
 
 if __name__ == "__main__":
     main()
